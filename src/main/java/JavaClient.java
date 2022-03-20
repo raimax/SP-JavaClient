12b2b1fd342459d4e4ac6dca3ab13083c90a77fa
raimax <4miner44@gmail.com>
2022-03-21 00:10:50 +0200

    commit

--- src/main/java/JavaClient.java
+++ src/main/java/JavaClient.java
@@ -1,25 +1,50 @@
 import java.net.*;
 import java.io.*;
 
+/**
+ * This class lets you connect to a tcp server
+ */
 public class JavaClient {
     private Socket clientSocket;
-    private PrintWriter out;
-    private BufferedReader in;
+    private BufferedOutputStream  out;
+    private BufferedInputStream in;
 
-    public void startConnection(String ip, int port) throws java.io.IOException {
+    /**
+     * Connects to server with specified ip and port address
+     * @param ip server ip address
+     * @param port server port address
+     * @throws IOException
+     */
+    public void startConnection(String ip, int port) throws IOException {
         clientSocket = new Socket(ip, port);
-        out = new PrintWriter(clientSocket.getOutputStream(), true);
-        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        System.out.println(String.format("Connected to %s:%d", ip, port));
+        out = new BufferedOutputStream(new FileOutputStream("blog.xml"));
+        in = new BufferedInputStream(new BufferedInputStream(clientSocket.getInputStream()));
+
+        receiveFile();
     }
 
-    public void stopConnection() throws java.io.IOException {
-        in.close();
-        out.close();
-        clientSocket.close();
+    private void receiveFile() throws IOException {
+        byte[] b = new byte[8 * 1024];
+
+        int len;
+        while ((len = in.read(b)) != -1) {
+            out.write(b, 0, len);
+        }
+
+        System.out.println("File received");
     }
 
-    public String sendMessage(String message) throws java.io.IOException {
-        out.println(message);
-        return in.readLine();
+    public void stopConnection() {
+        if (clientSocket.isConnected()) {
+            try {
+                in.close();
+                out.close();
+                clientSocket.close();
+            }
+            catch (IOException ex) {
+                System.out.println("Error stopping connection: " + ex.getMessage());
+            }
+        }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,11 +4,12 @@ public class Main {
 
         try {
             javaClient.startConnection("127.0.0.1", 6666);
-            String response = javaClient.sendMessage("hello java server");
-            System.out.println("Server: " + response);
         }
-        catch (Exception e) {
-            e.printStackTrace();
+        catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        finally {
+            javaClient.stopConnection();
         }
     }
 }
